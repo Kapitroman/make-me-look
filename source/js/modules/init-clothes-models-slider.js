0e49547c21386ede3d37cd235dbaacf19dd81e92
Kapitonov Roman <kapitroman@yandex.ru>
2024-03-16 19:26:46 +0300

    верстает третий экран

--- source/js/modules/init-clothes-models-slider.js
+++ source/js/modules/init-clothes-models-slider.js
@@ -1,6 +1,5 @@
 import {matchMedia} from './matchMedia';
 
-//const breakpoint = window.matchMedia('(max-width:479px)');
 const clothesItemShowContainer = document.querySelector('.clothes-item-show');
 let swiper, swiper2, swiper3;
 
@@ -11,7 +10,7 @@ const initLandscapeSwiper = (slider, slider2) => {
     slidesPerView: 1,
     spaceBetween: 20,
     pagination: {
-      el: '.swiper-pagination',
+      el: clothesItemShowContainer.querySelector('.swiper-pagination'),
       clickable: true,
     },
   });
@@ -70,77 +69,6 @@ const initClothesModelsSlider = () => {
     }
     initPortraitSwiper(modelsSlider);
   });
-
-  /*
-  const breakpointChecker = () => {
-    if (breakpoint.matches) {
-      if (swiper) {
-        swiper.destroy(true, true);
-      }
-    } else {
-      initSwiper(sliderStagesProject);
-    }
-  };
-
-  breakpoint.addListener(breakpointChecker);
-  breakpointChecker();
-  */
 };
 
 export {initClothesModelsSlider};
-/*
-const breakpoint = window.matchMedia('(max-width:479px)');
-const sliderStagesProject = document.querySelector('.project-stages__content');
-let swiper;
-
-const initSwiper = (slider) => {
-  // eslint-disable-next-line no-undef
-
-  swiper = new Swiper(slider, {
-
-    direction: "vertical",
-      slidesPerView: 1,
-      spaceBetween: 14,
-  });
-
-  const controlsStages = Array.from(document.querySelectorAll('.project-stages__control'));
-
-  const onClickControlStage = (evt) => {
-    const indexButton = evt.target.closest('button').getAttribute('data-stage');
-    swiper.slideTo(indexButton, 800);
-  };
-
-  controlsStages.forEach((item) => {
-    item.addEventListener('click', onClickControlStage);
-  })
-
-  const setActiveClass = () => {
-    const index = swiper.activeIndex;
-    controlsStages.forEach((item) => item.classList.remove('is-active'));
-    controlsStages[index].classList.add('is-active');
-  }
-
-  swiper.on('realIndexChange', setActiveClass);
-};
-
-const initStagesProjectSlider = () => {
-  if (!sliderStagesProject ) {
-    return;
-  }
-
-  const breakpointChecker = () => {
-    if (breakpoint.matches) {
-      if (swiper) {
-        swiper.destroy(true, true);
-      }
-    } else {
-      initSwiper(sliderStagesProject);
-    }
-  };
-
-  breakpoint.addListener(breakpointChecker);
-  breakpointChecker();
-};
-
-export {initStagesProjectSlider};
-*/
