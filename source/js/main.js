8dbee397dc1b18acc0a1d99a8447f9848c561030
Kapitonov Roman <kapitroman@yandex.ru>
2024-03-27 16:41:21 +0300

    добавляет радиокнопки переключения для al-assistant

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
 import {mobileVhFix} from './utils/mobile-vh-fix.js';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-import {CustomSelect} from './modules/select/custom-select';
-import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
+//import {initModals} from './modules/modals/init-modals';
+//import {Form} from './modules/form-validate/form';
+//import {CustomSelect} from './modules/select/custom-select';
+//import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
 
 
 //import {getMatchMedia} from './modules/get-match-media';
@@ -28,14 +28,14 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    uploadFile();
-    uploadImageDrop();
-    const select = new CustomSelect();
-    select.init();
-    const form = new Form();
-    window.form = form;
-    form.init();
+    //initModals();
+    //uploadFile();
+    //uploadImageDrop();
+    //const select = new CustomSelect();
+    //select.init();
+    //const form = new Form();
+    //window.form = form;
+    //form.init();
 
 
     //getMatchMedia();
--- source/js/modules/choose-look.js
+++ source/js/modules/choose-look.js
@@ -1,12 +1,21 @@
 const chooseLook = () => {
-  const controlls = document.querySelector('.al-assistant__viewer-controls-container');
+  const viewer = document.querySelector('.al-assistant__viewer');
 
-  if (!controlls) {
+  if (!viewer) {
     return;
   }
 
-  const pictures = document.querySelectorAll('.al-assistant__model');
-  const pictureButtons = document.querySelectorAll('.al-assistant__viewer-button');
+
+  const pictures = viewer.querySelectorAll('.al-assistant__model');
+  const controllButtons = viewer.querySelector('.al-assistant__viewer-buttons-container');
+  const controllRadios = viewer.querySelector('.al-assistant__viewer-radio-container');
+  const pictureButtons = viewer.querySelectorAll('.al-assistant__viewer-button');
+  const radioButtons = viewer.querySelectorAll('.al-assistant__viewer-radio-container input');
+  const links = viewer.querySelectorAll('.al-assistant__viewer-links-container img');
+  radioButtons[1].setAttribute('checked', true);
+  radioButtons[1].checked = true;
+  console.log(radioButtons);
+  console.log(radioButtons[1]);
 
   const clickPictureButtonHandler = (evt) => {
     evt.preventDefault();
@@ -19,12 +28,31 @@ const chooseLook = () => {
     }
     const index = target.getAttribute('data-index');
     pictures.forEach((item) => item.classList.remove('is-active'));
-    pictures[index].classList.add('is-active');
+    radioButtons.forEach((item) => item.removeAttribute('checked'));
+    links.forEach((item) => item.classList.remove('is-active'));
     pictureButtons.forEach((item) => item.classList.remove('is-active'));
+    pictures[index].classList.add('is-active');
+    radioButtons[index].setAttribute('checked', true);
+    links[index].classList.add('is-active');
     pictureButtons[index].classList.add('is-active');
   }
 
-  controlls.addEventListener('click', clickPictureButtonHandler);
+  const changeRadioButtonHandler = (evt) => {
+
+    const target = evt.target.closest('label').querySelector('input');
+    const index = target.getAttribute('data-radio-index');
+    pictures.forEach((item) => item.classList.remove('is-active'));
+    radioButtons.forEach((item) => item.removeAttribute('checked'));
+    links.forEach((item) => item.classList.remove('is-active'));
+    pictureButtons.forEach((item) => item.classList.remove('is-active'));
+    pictures[index].classList.add('is-active');
+    radioButtons[index].setAttribute('checked', true);
+    links[index].classList.add('is-active');
+    pictureButtons[index].classList.add('is-active');
+  }
+
+  controllButtons.addEventListener('click', clickPictureButtonHandler);
+  controllRadios.addEventListener('change', changeRadioButtonHandler);
 }
 
 export {chooseLook};
